032c922e3e077423c54de17330b8996b58f7de07
Rizky Maulana Nugraha <rizky.maulana@rotterdam-cs.com>
2014-05-24 10:57:33 +0700

    Hackathon preparation test

---
+++ safe_qgis/hackathon_training/test_working_with_vector.py
@@ -0,0 +1,138 @@
+__author__ = 'maul'
+__author__ = 'gumbia'
+
+import os
+import unittest
+
+from qgis.core import QgsVectorLayer
+from safe_qgis.hackathon_training.working_with_vector import (
+    load_layer,
+    iterate_data,
+    modify_attribute_value,
+    add_string_attribute,
+    delete_attribute,
+    write_layer_to_shapefile)
+
+
+class TestWorkingWithVector(unittest.TestCase):
+    def test_load_layer(self):
+        """Test the load_layer function works."""
+        vector_path = os.path.abspath(
+            os.path.join(
+                os.path.dirname(__file__),
+                '../../safe/test/data/boundaries/district_osm_jakarta.shp'))
+        layer = load_layer(vector_path, 'District OSM Jakarta')
+        message = 'It is not a QgsVectorLayer instance :('
+        self.assertTrue(isinstance(layer, QgsVectorLayer), message)
+        message = 'Hmm, something is wrong here, the number of features ' \
+                  'should be 5'
+        self.assertEqual(layer.featureCount(), 5, message)
+
+        # Let's mess it up with wrong path
+        vector_path = os.path.abspath(
+            os.path.join(
+                os.path.dirname(__file__),
+                '../../safe/test/data/boundaries/this_file_is_a_fool.shp'))
+        layer = load_layer(vector_path, 'District OSM Jakarta')
+        message = 'Dude! You are doing something wrong here'
+        self.assertFalse(layer, message)
+
+    def test_iterate_data(self):
+        """Test the iterate_data function works."""
+        # Load the layer first
+        vector_path = os.path.join(
+            os.path.dirname(__file__),
+            '../../safe/test/data/boundaries/district_osm_jakarta.shp')
+        layer = load_layer(vector_path, 'District OSM Jakarta')
+        # Iterate
+        iterate_data(layer)
+
+    def test_modify_data(self):
+        """Test the modify_data works."""
+        # Load the layer first
+        vector_path = os.path.join(
+            os.path.dirname(__file__),
+            '../../safe/test/data/boundaries/district_osm_jakarta.shp')
+        layer = load_layer(vector_path, 'District OSM Jakarta')
+
+        # Add the attribute 'modify'
+        add_string_attribute(layer, 'modify')
+
+        # Print it
+        print 'Before modified:'
+        iterate_data(layer)
+
+        # Now modify all the 'modify' attributes
+        modify_attribute_value(layer, 'modify')
+
+        # Print it
+        print 'After modified'
+        iterate_data(layer)
+
+        # Clean the state delete the attribute 'modify' again
+        delete_attribute(layer, 'modify')
+
+
+    def test_add_string_attribute(self):
+        """Test the add_string_attribute function works."""
+        # Load the layer first
+        vector_path = os.path.join(
+            os.path.dirname(__file__),
+            '../../safe/test/data/boundaries/district_osm_jakarta.shp')
+        layer = load_layer(vector_path, 'District OSM Jakarta')
+
+        # Add an attribute called 'tes_add'
+        add_string_attribute(layer, 'tes_add')
+        provider = layer.dataProvider()
+        attribute_names = []
+        fields = provider.fields()
+        for field in fields:
+            attribute_names.append(field.name())
+        print 'The fields now after adding tes_add: %s' % attribute_names
+
+        # Delete the attribute again OK, so we're back to the clean state
+        delete_attribute(layer, 'tes_add')
+
+        provider = layer.dataProvider()
+        attribute_names = []
+        fields = provider.fields()
+        for field in fields:
+            attribute_names.append(field.name())
+        print 'The fields now after deleting tes_add: %s' % attribute_names
+
+    def test_delete_attribute(self):
+        """Test the delete_attribute function works."""
+        # Load the layer first
+        vector_path = os.path.join(
+            os.path.dirname(__file__),
+            '../../safe/test/data/boundaries/district_osm_jakarta.shp')
+        layer = load_layer(vector_path, 'District OSM Jakarta')
+
+        # Add an attribute called 'tes_delete'
+        add_string_attribute(layer, 'delete')
+
+        # Delete it again
+        delete_attribute(layer, 'delete')
+
+    def test_write_layer_to_shapefile(self):
+        """Test the write_layer_to_shapefile works."""
+        # Load layer
+        vector_path = os.path.join(
+            os.path.dirname(__file__),
+            '../../safe/test/data/boundaries/district_osm_jakarta.shp')
+        layer = load_layer(vector_path, 'District OSM Jakarta')
+
+        # Add an attribute called 'hackathon' to layer
+        add_string_attribute(layer, 'hackathon')
+
+        # Save it to new shapefile
+        new_layer_path = '/tmp/hackathon.shp'
+        path = write_layer_to_shapefile(layer, new_layer_path)
+
+        # There should be exist /tmp/hackthon.shp file
+        self.assertTrue(os.path.exists(path))
+
+        # Delete hackathon attribute again to make the layer back to clean
+        # state
+        delete_attribute(layer, 'hackathon')
+
---
+++ safe_qgis/hackathon_training/working_with_vector.py
@@ -0,0 +1,160 @@
+__author__ = 'maul'
+
+# coding=utf-8
+"""
+InaSAFE Disaster risk assessment tool developed by AusAid
+
+Contact : ole.moller.nielsen@gmail.com
+
+.. note:: This program is free software; you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published by
+     the Free Software Foundation; either version 2 of the License, or
+     (at your option) any later version.
+
+"""
+__author__ = 'gumbia'
+
+from PyQt4.QtCore import QVariant
+
+from safe.common.testing import get_qgis_app
+from qgis.core import (
+    QgsVectorLayer,
+    QgsVectorDataProvider,
+    QgsField,
+    QgsVectorFileWriter)
+
+
+QGIS_APP, CANVAS, IFACE, PARENT = get_qgis_app()
+
+
+def load_layer(data_source, layer_name):
+    """Load a vector layer with ogr provider.
+
+    :param data_source: The path to the data source.
+    :type data_source: str
+
+    :param layer_name: The name of the soon-to-be our loaded layer.
+    :type layer_name: str
+
+    :return: A layer of QgsVectorLayer if it is successfully loaded.
+        Or False if it fails.
+    :rtype: QgsVectorLayer, bool
+    """
+    layer = QgsVectorLayer(data_source, layer_name, 'ogr')
+    if layer.isValid():
+        return layer
+    return False
+
+
+def add_string_attribute(layer, attribute_name):
+    """Add a string attribute in vector layer with specific name.
+
+    :param layer: The layer that we will work on.
+    :type layer: QgsVectorLayer
+
+    :param attribute_name: The name of the attribute. Remember that ESRI
+        shapefile restricts the length of the attribute name to be less than
+        or equal to 10.
+    :type attribute_name: str
+    """
+    layer.startEditing()
+    provider = layer.dataProvider()
+    capabilities = provider.capabilities()
+
+    if capabilities & QgsVectorDataProvider.AddAttributes:
+        result = provider.addAttributes(
+            [QgsField(attribute_name, QVariant.String)])
+        if not result:
+            raise Exception(
+                'Oh no, there is something wrong when adding this new '
+                'attribute:' % attribute_name)
+    layer.commitChanges()
+
+
+def delete_attribute(layer, attribute_name):
+    """Delete an attribute with given index.
+
+    :param layer: The target we are working on.
+    :type layer: QgsVectorLayer
+
+    :param attribute_name: The attribute name that are going to be deleted.
+    :type attribute_name: str
+    """
+    layer.startEditing()
+    provider = layer.dataProvider()
+    capabilities = provider.capabilities()
+
+    attribute_index = provider.fieldNameIndex(attribute_name)
+    if attribute_index == -1:
+        raise Exception(
+            'Dude, you are lying to me by giving wrong attribute name!')
+
+    if capabilities & QgsVectorDataProvider.DeleteAttributes:
+        layer.dataProvider().deleteAttributes([attribute_index])
+    layer.commitChanges()
+
+
+def iterate_data(layer):
+    """Iterate all features in the layer.
+
+    :param layer: The layer that we're working on.
+    :type layer: QgsVectorLayer
+    """
+    features = layer.getFeatures()
+    for feature in features:
+        # fetch attributes
+        attributes = feature.attributes()
+
+        # attributes is a list.
+        # It contains all the attribute values of this feature
+        print attributes
+
+
+def modify_attribute_value(layer, attribute_name):
+    """Modify the attribute value. We'll just change the value of the
+        attribute all to 'modif'
+
+    :param layer: The layer we're working on.
+    :type layer: QgsVectorLayer
+
+    :param attribute_name: The attribute name that will be modified.
+    :type attribute_name: str
+    """
+    layer.startEditing()
+    provider = layer.dataProvider()
+    capabilities = provider.capabilities()
+
+    attribute_index = provider.fieldNameIndex(attribute_name)
+    if attribute_index == -1:
+        raise Exception(
+            'Dude, you are lying to me by giving wrong attribute name!')
+
+    if capabilities & QgsVectorDataProvider.ChangeAttributeValues:
+        features = layer.getFeatures()
+        for feature in features:
+            feature_id = feature.id()
+            attributes = {attribute_index: 'modif'}
+            layer.dataProvider().changeAttributeValues({feature_id: attributes})
+    layer.commitChanges()
+
+
+def write_layer_to_shapefile(layer, path):
+    """Write layer to a shapefile with given path.
+
+    :param layer: The layer that will be saved.
+    :type layer: QgsVectorLayer
+
+    :param path: The file path.
+    :type path: str
+
+    :return: Path to the result file if it's successful. Or else return False
+    :rtype: str, bool
+    """
+    error = QgsVectorFileWriter.writeAsVectorFormat(
+        layer, path, "CP1250", None, "ESRI Shapefile")
+
+    if error == QgsVectorFileWriter.NoError:
+        return path
+
+    return False
+
